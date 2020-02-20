574028bc08778c8751d5519ea98d6a919903d309
t-mocho <55965167+t-mocho@users.noreply.github.com>
2020-02-20 17:23:20 +0900

    保存無完成

--- app.js
+++ app.js
@@ -1,95 +1,56 @@
-// todolist
-var todos = [{
-  num: 0,
-  task: addtext,
-  done: false
-}];
-// 0番目削除
-todos.shift();
+// 出力
+const addBtn = document.getElementById('inBtn');
+const todos = [];
+const addTask = document.getElementById('task-list');
 
-// id
-const tasklist = document.getElementById('tasklist');
+// ボタン押下で追加
+addBtn.onclick = function() {
+  const addText = document.getElementById('inArea');
+  todos.push(addText.value);
 
-// 追加
-const addbtn = document.getElementById('addbtn');
-addbtn.onclick = function() {
-  const text = document.getElementById('addtext').value;
-  const todo = {};
-  if (text) {
-    // 値更新
-    todo.task = text;
-    todo.done = false;
-    // 配列追加
-    todos.push(todo);
-    // 書き出し'
-    addtodo(todos);
-    // 入力値リセット
-    let re = document.getElementById('addtext');
-    re.value = "";
+  // 空文字は入力値スルー
+  if (addText.value !== "") {
+    // li作成
+    var addLi = document.createElement('li');
+    for (var i = 0; i < todos.length; i++) {
+      addLi.innerHTML = todos[i];
+      document.getElementById('task-list').appendChild(addLi);
+    }
+    // アイコン追加
+    addLi.insertAdjacentHTML('afterbegin', '<i class="material-icons add">favorite</i>');
+    addLi.insertAdjacentHTML('beforeend', '<i class="material-icons del">delete</i>');
   }
-};
+  // 入力値リセット
+  addText.value = "";
+}
 
-// 書き出し関数
-function addtodo(todos) {
-  // li作成
-  var newli = document.createElement('li');
-  // todosの配列を1つずつとる
-  for (let i in todos) {
-    var addlist = todos[i].task;
+
+// 削除
+const delList = document.getElementById('task-list');
+delList.addEventListener('click', (e) => {
+  let target = e.target;
+  const delBtn = document.getElementsByClassName('material-icons del');
+  for (var i = 0; i < delBtn.length; i++) {
+    if (target === delBtn[i]) {
+      const delList = target.parentNode;
+      delList.style.transform = "translateX(150px)";
+      delList.style.opacity = "0";
+      delList.style.transition = ".9s";
+      setTimeout(function() {
+        delList.style.display = "none";
+      }, 800);
+    }
   }
-  // HTMLに書き出し
-  newli.insertAdjacentHTML('afterbegin', '<i class="material-icons add">favorite</i>');
-  newli.appendChild(document.createTextNode(addlist));
-  newli.insertAdjacentHTML('beforeend', '<i class="material-icons del">delete</i>');
-  tasklist.appendChild(newli);
-};
+});
 
 // 完了
-tasklist.addEventListener('click', (e) => {
-  //console.log(todos);
+document.getElementById('task-list').addEventListener('click', (e) => {
   let target = e.target;
-  // addclass
-  const icon = document.getElementsByClassName('material-icons add');
-  if (target.classList.contains('add')) {
-    for (var i = 0; i < todos.length; i++) {
-      // ハートクリックでtoggle
-      if (target === icon[i]) {
-        target.classList.toggle('end');
-        // done変更
-        todos[i].done = !todos[i].done;
-      }
+  const endLi = document.getElementsByTagName('li');
+  const endImg = document.getElementsByClassName('material-icons add');
+  for (var i = 0; i < endImg.length; i++) {
+    if (target === endImg[i]) {
+      target.classList.toggle('end');
     }
   }
 });
-
-tasklist.addEventListener('click', (a) => {
-  target = a.target;
-  for (var i = 0; i < todos.length; i++) {
-    const delicon = document.getElementsByClassName('material-icons del');
-    // deliconがクリックされたら処理
-    if (target === delicon[i]) {
-      // 番号取得
-      var id = todos.indexOf(todos[i]);
-      // 配列削除
-      todos.splice(id, 1);
-      // HTML削除
-      target.parentElement.remove();
-    }
-  }
-});
-
-// ローカルに保存ッ
-const savebtn = document.getElementById('savebtn');
-savebtn.onclick = function() {
-  //JSON.stringifyした上で、localStorageに保存します。
-  var deta = localStorage.setItem('todos', JSON.stringify(todos));
-};
-
-// ローカルから取り出し
-(function() {
-  //localStorageから取り出し
-  var loadtodo = localStorage.getItem('todos');
-  //取り出した後にJSON.parseをかけます。
-  tododeta = JSON.parse(loadtodo);
-  console.log(tododeta);
-})();
